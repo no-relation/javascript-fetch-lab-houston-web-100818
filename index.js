07e75f90d3888d8f530d76b6228d5b76202c4490
no-relation <eddiec76@gmail.com>
2018-11-26 14:00:55 -0600

    404 error

--- index.js
+++ index.js
@@ -13,10 +13,16 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}`, {
+    method: 'POST',
+    headers: {
+      Authorization: getToken()
+    }
+  }).then( console.log )
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '28487438de51f773f54ca88273488de3a4e8ca58'
 }
